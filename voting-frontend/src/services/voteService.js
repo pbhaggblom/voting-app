39f53c1cdf9ecaf850118eed481b3a60df4c4570
Pontus Häggblom <pbhaggblom@gmail.com>
2026-04-05 13:18:10 +0200

    Add method for getting initial results

--- voting-frontend/src/services/voteService.js
+++ voting-frontend/src/services/voteService.js
@@ -1,8 +1,20 @@
-const API_URL = 'http://localhost:8080/vote'
-const WS_URL = 'ws://localhost:8080/results'
+const API_URL = '/vote'
+
+const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:'
+const WS_URL = `${protocol}//${window.location.host}/results`
 
 export const postVote = (option) => {
   return fetch(`${API_URL}/${option}`, { method: 'POST' })
+}
+
+export const getInitialResults = async () => {
+  try {
+    const response = await fetch(`${API_URL}/results`)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error('Could not fetch data:', error)
+  }
 }
 
 export const connectToResults = (onMessage) => {
